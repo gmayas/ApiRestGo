1ef43627098f108cef3be29edffcc1107c26552e
Gabriel Maya Sánchez. <isc_gmayas@hotmail.com>
2025-08-18 21:52:34 -0600

    Methos User, Task commit

--- main.go
+++ main.go
@@ -20,8 +20,17 @@ func main() {
 	r := mux.NewRouter()
 	// Define the home route
 	r.HandleFunc("/", routes.HomeHandler).Methods("GET")
+	// Define user routes
+	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", routes.CreateUserHandler).Methods("POST")
+	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+	// Define task routes
+	r.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	r.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
 	// Start the server, port 3000
 	fmt.Println("ApiRestGo server run port: 3000.")
 	http.ListenAndServe(":3000", r)
-
 }
--- src/routes/users.routes.go
+++ src/routes/users.routes.go
@@ -1,1 +1,61 @@
 package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gmayas/ApiRestGo/src/db"
+	"github.com/gmayas/ApiRestGo/src/models"
+	"github.com/gorilla/mux"
+)
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	// Handler logic to get users
+	var users []models.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Handler logic to get 
+	params := mux.Vars(r)
+	var user models.User
+	db.DB.First(&user, params["id"])
+	if user.ID == 0 {	
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found"))
+		return
+	}
+    db.DB.Model(&user).Association("Tasks").Find(&user.Tasks)
+	json.NewEncoder(w).Encode(&user)
+}
+
+func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Handler logic to create a user
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	createdUser := db.DB.Create(&user)
+	err := createdUser.Error
+	if err != nil {	
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Error creating user: " + err.Error()))
+		return		
+	}
+	
+	json.NewEncoder(w).Encode(&user)
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var user models.User
+	db.DB.First(&user, params["id"])
+
+	if user.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found"))
+		return
+	}
+
+	db.DB.Unscoped().Delete(&user)
+	w.WriteHeader(http.StatusOK)
+}
